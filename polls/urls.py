e60ff54b771938bfb9e261926e92e4b40e93a75b
Alfarabi09 <fara2001@mail.ru>
2023-08-08 18:00:23 +0600

    calc

--- polls/urls.py
+++ polls/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path('<int:first>/add/<int:second>/', views.add, name='add'),
     path('transform/<str:text>/', views.upp, name='upp'),
-    path('check/<str:word>/', views.is_palindrome, name='is_palindrome')
+    path('check/<str:word>/', views.is_palindrome, name='is_palindrome'),
+    path('calc/<int:first>/<str:operation>/<int:second>/', views.calculator, name='calculator'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, nfactorial")
@@ -15,3 +15,20 @@ def is_palindrome(request, word):
         if word == word[::-1]:
             return HttpResponse("True")
         return HttpResponse("False")
+
+def calculator(request, first, operation, second):
+        if operation == 'add':
+            result = first + second
+        elif operation == 'sub':
+            result = first - second
+        elif operation == 'mult':
+            result = first * second
+        elif operation == 'div':
+            if second != 0:
+                result = first / second
+            else:
+                return HttpResponseBadRequest("Cannot divide by zero")
+        else:
+            return HttpResponseBadRequest("Invalid operation")
+
+        return HttpResponse(str(result))
